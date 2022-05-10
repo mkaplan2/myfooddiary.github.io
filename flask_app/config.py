b097af85a9856baff0dbaf36bf32d72406244ec3
Maya Kaplan <mkaplan2@terpmail.umd.edu>
2022-05-10 08:35:25 -0400

    make it better

--- flask_app/config.py
+++ flask_app/config.py
@@ -2,3 +2,4 @@
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
 #MONGODB_HOST = "mongodb://localhost:27017/final_project" #Comment back in if need to
 MONGODB_HOST = "mongodb+srv://admin_user:ZOS242hRMvGjXLse@cluster0.m04uq.mongodb.net/Cluster0?retryWrites=true&w=majority"
+MONGODB_URI = "mongodb+srv://admin_user:ZOS242hRMvGjXLse@cluster0.m04uq.mongodb.net/Cluster0?retryWrites=true&w=majority"
